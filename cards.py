45865496341cca00889a8660c3876522e529f11c
EdmundGoodman <egoodman3141@gmail.com>
2020-10-24 23:54:54 +0100

    Added a Card object, with methods to get the suit and the face, and a large number of dunder methods to help model interactions between the Card and other objects

--- cards.py
+++ cards.py
@@ -22,3 +22,51 @@ class Faces(enum.Enum):
     JACK = 11
     QUEEN = 12
     KING = 13
+
+
+
+class Card:
+    def __init__(self, suit, face):
+        self.suit = suit
+        self.face = face
+
+    def getSuit(self):
+        return self.suit
+
+    def getFace(self):
+        return self.face
+
+    def __eq__(self, other):
+        return self.suit == other.suit and self.face == other.face
+
+    def __ne__(self, other):
+        return self == other
+
+    def __lt__(self, other):
+        if self.face < other.face:
+            return True
+        else:
+            return self.suit < other.suit
+
+    def __le__(self, other):
+        return self < other or self == other
+
+    def __gt__(self, other):
+        if self.face > other.face:
+            return True
+        else:
+            return self.suit > other.suit
+
+    def __ge__(self, other):
+        return self > other or self == other
+
+    def __hash__(self):
+        return int(str(self.suit.value)+str(self.face.value))
+
+    def __str__(self):
+        faceChars = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",")
+        suitChars = '♦,♣,♥,♠'.split(",")
+        return faceChars[self.face.value-1] + suitChars[self.suit.value-1]
+
+    def __repr__(self):
+        return str(self)
