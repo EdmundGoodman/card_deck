7dd9e4f190ca679d74f73e5bb373fc1bf53f9a43
EdmundGoodman <egoodman3141@gmail.com>
2020-10-24 23:53:36 +0100

    Added enums for Suits and Faces

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cardModel
+# Card Model
 An object model of a pack of cards, written in Python, to streamline writing programs to model/play games involving cards
---
+++ cards.py
@@ -0,0 +1,24 @@
+import enum
+
+
+class Suits(enum.Enum):
+    DIAMONDS = 1
+    CLUBS = 2
+    HEARTS = 3
+    SPADES = 4
+
+
+class Faces(enum.Enum):
+    ACE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
