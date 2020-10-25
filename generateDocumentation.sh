c405c8fb51acbf54b1272a8fd21151dd658f8d16
EdmundGoodman <egoodman3141@gmail.com>
2020-10-25 23:13:46 +0000

    Fixed generateDocumentation.sh to produce valid link to source files

--- generateDocumentation.sh
+++ generateDocumentation.sh
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 find ./ -name '*.py' | xargs python3 -m pydoc -w $
-find ./ -name '*.html' | xargs sed -i -E 's/<a href=\"file:.*\/cardModel\/(.*)\">.*\/cardModel\/.*<\/a>/<a href=\"file:\.\/cardModel\/\1\">\.\/cardModel\/\1<\/a>/g'
+find ./ -name '*.html' | xargs sed -i -E 's/<a href=\"file:.*\/cardModel\/(.*)\">.*\/cardModel\/.*<\/a>/<a href=\"file:\.\/\1\">\.\/\1<\/a>/g'
 
