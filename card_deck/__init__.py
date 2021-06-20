4b4a6a4eb96bc7a692d6b35d8d5e444e0fb80c77
EdmundGoodman <egoodman3141@gmail.com>
2021-06-20 19:46:29 +0100

    Further renamed files and moved towards pypi compatible stuff

---
+++ card_deck/__init__.py
@@ -0,0 +1,1 @@
+name = "card_deck"
--- generateDocumentation.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-# -*- coding: utf-8 -*-
-
-find ./ -name '*.py' | xargs python3 -m pydoc -w $
-find ./ -name '*.html' | xargs sed -i -E 's/<a href=\"file:.*\/cardModel\/(.*)\">.*\/cardModel\/.*<\/a>/<a href=\"file:\.\/\1\">\.\/\1<\/a>/g'
-
