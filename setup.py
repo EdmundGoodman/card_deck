228d96593eb6ef1351f0f7abd4dc491a949590ba
EdmundGoodman <egoodman3141@gmail.com>
2021-06-21 17:50:08 +0100

    Fixed valid python versions to support

--- setup.py
+++ setup.py
@@ -6,20 +6,23 @@ with open("README.md", "r") as fh:
 setup(
     name = 'card_deck',
     packages = ['card_deck'],
-    version = '1.0.0',
+    version = '1.0.1',
     license='MIT',
     description = 'An object model of a pack of cards',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author = 'Edmund Goodman',
     url = 'https://github.com/EdmundGoodman/card_deck',
-    download_url = 'https://github.com/EdmundGoodman/card_deck/archive/refs/tags/v1.0.0.tar.gz',        # I explain this later on
+    download_url = 'https://github.com/EdmundGoodman/card_deck/archive/refs/tags/v1.0.1.tar.gz',        # I explain this later on
     keywords = ['game', 'card', 'model'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8.5',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
