bbb0d35176ce03500cd6174478834fef09c7a7a3
EdmundGoodman <egoodman3141@gmail.com>
2021-06-21 17:40:30 +0100

    Started readying for upload to pypi, writing setup and __init__ files

--- card_deck/__init__.py
+++ card_deck/__init__.py
@@ -1,1 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from card_deck import Faces, Suits, Card, Pile, Deck, CardError, PileError
 name = "card_deck"
--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-import setuptools
+from distutils.core import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="card_deck",
-    version="1.0.0",
-    author="Edmund Goodman",
-    description="An object model of a pack of cards",
+setup(
+    name = 'card_deck',
+    packages = ['card_deck'],
+    version = '1.0.0',
+    license='MIT',
+    description = 'An object model of a pack of cards',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/EdmundGoodman/cards",
-    packages=setuptools.find_packages(),
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+    author = 'Edmund Goodman',
+    url = 'https://github.com/EdmundGoodman/card_deck',
+    download_url = 'https://github.com/EdmundGoodman/card_deck/archive/v_01.tar.gz',        # I explain this later on
+    keywords = ['game', 'card', 'model'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8.5',
+    ],
 )
