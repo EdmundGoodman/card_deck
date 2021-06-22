349cca36127e37b9b20735760846b6d66b4511bd
EdmundGoodman <egoodman3141@gmail.com>
2021-06-22 15:31:53 +0100

    Tried suggested fix

--- card_deck/__init__.py
+++ card_deck/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from model import Suits, Faces, CardError, PileError, Card, Pile, Deck
+from .model import Suits, Faces, CardError, PileError, Card, Pile, Deck
