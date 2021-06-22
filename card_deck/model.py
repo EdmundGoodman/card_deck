1c3b25cba5a959b6dce285a8c567ebf5d3610374
EdmundGoodman <egoodman3141@gmail.com>
2021-06-23 00:33:48 +0100

    Fixed pile repr to better indicate dealing

--- card_deck/model.py
+++ card_deck/model.py
@@ -320,7 +320,7 @@ class Pile:
     def __str__(self):
         """Use the string representation of the pile as the informal
         representation of the pile"""
-        return ", ".join([str(x) for x in self._cards])
+        return "[" + ", ".join([str(x) for x in self._cards]) + "]"
 
     def __repr__(self):
         """Return a string representation of the pile, formatted as a list
@@ -336,39 +336,3 @@ class Deck(Pile):
         for suit in Suits:
             for face in Faces:
                 self._cards.append(Card(face, suit))
-
-
-if __name__ == "__main__":
-    import copy
-
-    # Test getting the typeable name of a card
-    print(Card(Faces.SIX, Suits.SPADES).get_typeable_name())
-
-    # Test gettting a card form its typeable name
-    while True:
-        inp = str(input("Enter the typeable name of a card: "))
-        card = Card.get_from_typeable_name(inp)
-        if card is not None:
-            break
-    print(card)
-
-    # Test making Decks and Pile
-    d1 = Deck()
-    d2 = Pile([Card(Faces.ACE, Suits.DIAMONDS)])
-    d3 = Pile([
-        Card(Faces.ACE, Suits.HEARTS),
-        Card(Faces.FIVE, Suits.CLUBS),
-    ])
-
-    # Test operations on Decks and Piles
-    print(dir(d1))
-    print(d1)
-    print(d2)
-    print(d1 - d2)
-    print(d1 ^ d2)
-    print(d1 and d3)
-    print(d1 is copy.deepcopy(d1))
-
-    # Test the hash functions of Pile objects
-    print(hash(d1))
-    print(hash(d2))
